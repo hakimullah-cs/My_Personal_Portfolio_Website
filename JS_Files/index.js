9b76e7cc84cb8bbdabcb02e8b4bb0cb22f32b500
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2023-04-20 16:42:10 +0500

    Making Responsive Header

---
+++ JS_Files/index.js
@@ -0,0 +1,12 @@
+//---/**********/Header Functonality JSCode Start /**************/--- 
+const menuicon=document.getElementById("menu"),
+      closeicon=document.getElementById("close"),
+      menulist=document.querySelector(".navbar-list"),
+      body=document.getElementById("body");
+menuicon.addEventListener("click",(e)=>{
+    menulist.classList.add("active");
+});
+closeicon.addEventListener("click",(e)=>{
+   menulist.classList.remove("active");
+});
+//---/**********/Header Functonality JSCode End /**************/--- */
