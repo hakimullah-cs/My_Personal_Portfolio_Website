ed5e229867a7b725f29b2ffdfe46d69c0ca8f2b1
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2023-05-03 18:58:42 +0500

    Adding animation in hero and Experinces section and add some functionality through JS

--- JS_Files/index.js
+++ JS_Files/index.js
@@ -1,15 +1,31 @@
 //---/**********/Header Functonality JSCode Start /**************/--- 
-const menuicon=document.getElementById("menu"),
+const  html=document.getElementById("root"),
+      menuicon=document.getElementById("menu"),
       closeicon=document.getElementById("close"),
       menulist=document.querySelector(".navbar-list"),
-      body=document.getElementById("body");
+      navlinks=document.querySelectorAll(".navbar-list .navbar-link"),
+      mainElement=document.querySelector(".main");
+
 menuicon.addEventListener("click",(e)=>{
     menulist.classList.add("active");
+    html.classList.add("stop-scroll");
+    mainElement.classList.add("show");
 });
 closeicon.addEventListener("click",(e)=>{
    menulist.classList.remove("active");
+   html.classList.remove("stop-scroll");
+   mainElement.classList.remove("show");
 });
+console.log(html);
 
+console.log(navlinks);
+navlinks.forEach((e)=>{
+e.addEventListener("click",(ele)=>{
+    html.classList.remove("stop-scroll");
+menulist.classList.remove("active");
+mainElement.classList.remove("show");
+  });
+});
 // ******* Sticky Header Start ***********
 let header=document.querySelector(".header");
 window.addEventListener("scroll",(e)=>{
@@ -23,3 +39,25 @@ header.classList.add("sticky");
 
 // ******* Sticky Header End ***********
 //---/**********/Header Functonality JSCode End /**************/--- */
+
+
+
+// /*********./ Main Section Functionality Start /***********/ */ */
+// --------------- Experience Section Start --------------
+const boxes=document.querySelectorAll(".experience-card");
+console.log(boxes);
+window.addEventListener("scroll",checkboxes);
+checkboxes();
+function checkboxes(){
+   boxes.forEach((box)=>{
+if(window.pageYOffset>=600){
+    // alert();
+    box.classList.add("show");
+}else{
+    box.classList.remove("show");
+}
+   });
+}
+
+// --------------- Experience Section End --------------
+// /*********./ Main Section Functionality End /***********/ */ */
